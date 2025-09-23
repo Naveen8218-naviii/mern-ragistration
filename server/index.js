79b1272dde4ae7b743a140e1ae2aa60af94b7fd9
Naveen8218-naviii <nc319631@gmail.com>
2025-09-23 15:10:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -49,6 +49,7 @@ app.post("/login",(req,res)=>{
     })
 })
 
-app.listen(5000,()=>{
-    console.log("wegi lo connect")
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
